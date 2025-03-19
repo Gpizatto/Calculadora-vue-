43fa041d4dd3b5ee5488cc904bb340728d848ddb
GUSTAVO PIZATTO <gustavopizatto@hotmail.com>
2025-03-19 08:32:13 -0300

    Add files via upload

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="button">
+        
+    </button>
+</template>
+
+<script>
+export default {
+name:"ButtonComponent"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main/Calculator.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="calculator">
+    <DisplayComponent/>
+    <ButtonComponent/>
+
+  
+    
+  </div>
+</template>
+
+<script>
+import ButtonComponent from"../components/Button.vue"
+import DisplayComponent from"../components/Display"
+
+
+export default {
+  name:"CalculatorComponent",
+
+    components: { ButtonComponent,DisplayComponent}
+
+}
+</script>
+
+
+<style>
+.calculator {
+    height: 320px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+</style>
